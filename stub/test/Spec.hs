7456be90306333b23f207defcbc93b5f3d65e3f1
Randall Britten <randall.britten@gmail.com>
2015-11-14 12:31:53 +1300

    QuickCheck now automatically finds all properties

--- stub/test/Spec.hs
+++ stub/test/Spec.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
+module Main where
+
 import Test.QuickCheck
+import Test.QuickCheck.All
 
 prop_revapp :: [Int] -> [Int] -> Bool
 prop_revapp xs ys = reverse (xs++ys) == reverse ys ++ reverse xs
 
-main :: IO ()
-main = quickCheck prop_revapp
+return []
+main = $quickCheckAll
